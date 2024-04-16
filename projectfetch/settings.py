c955bd08deb4c9a58182c6b88067ff201369b998
preethampy <preetham.web3@gmail.com>
2024-04-16 22:02:24 +0530

    Changed allowed hosts

--- projectfetch/settings.py
+++ projectfetch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^@egq^5=v)#*r)#9hjty8c3yxmg6v_0&2zpm8oss^b)tdn=%g+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
